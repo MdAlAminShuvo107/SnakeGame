dbe969b40467cb80bbf615271b1bdfe682da8612
shuvo <shuvo@Shuvo>
2019-05-11 09:34:56 +0600

    Second Commit

---
+++ 2Dsnake/src/Gameplay.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Gameplay extends JPanel{
+	private ImageIcon titleImage;
+
+	public Gameplay() 
+	{
+		
+		
+	}
+	
+	public void paint (Graphics g)
+	{
+		
+		
+	}
+	
+	
+
+}
--- 2Dsnake/src/Main.java
+++ 2Dsnake/src/Main.java
@@ -1,8 +1,17 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JFrame obj = new JFrame();
+		
+		obj.setBounds(10, 10, 905, 700);
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
